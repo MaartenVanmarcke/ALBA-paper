87c9f7123c77070b0886db81d77d31d4273a295d
Maarten Vanmarcke <maarten.vanmarcke@student.kuleuven.be>
2024-05-21 15:03:42 +0200

    sdf

--- src/pipeline/plotPerformance2.py
+++ src/pipeline/plotPerformance2.py
@@ -21,6 +21,7 @@ class PlotPerformance:
             aucroc = []
             aucrocbag = []
             directory = os.path.join(current, "results",self.name)
+            lastname = ""
             import csv
             for filename in os.listdir(directory):
                 f = os.path.join(directory, filename)
@@ -29,12 +30,20 @@ class PlotPerformance:
                     with open(f, 'r') as readFile:
                         reader = csv.reader(readFile)
                         lines = list(reader)
-                        names.append(f.split("\\")[-1].split(".")[0])
-                        for line in lines:
-                            if line[0] == "auc_roc":
-                                aucroc.append(line[1:])
-                            if line[0] == "auc_roc_bag":
-                                aucrocbag.append(line[1:])
+                        if names[-1] == lastname:
+                            for line in lines:
+                                if line[0] == "auc_roc":
+                                    aucroc[-1] = aucroc[-1] + line[1:]
+                                if line[0] == "auc_roc_bag":
+                                    aucrocbag[-1] = aucrocbag[-1] + line[1:]
+                        else:
+                            names.append(f.split("\\")[-1].split(".")[0])
+                            for line in lines:
+                                if line[0] == "auc_roc":
+                                    aucroc.append(line[1:])
+                                if line[0] == "auc_roc_bag":
+                                    aucrocbag.append(line[1:])
+                            lastname = names[-1]
             plt.figure()
             for i in range(len(names)):
                 plt.plot(np.arange(0,len(aucroc[i])),[float(k) for k in aucroc[i]], label = names[i])
@@ -56,98 +65,6 @@ class PlotPerformance:
             plt.grid()
             plt.show()
             return
-            fig, ax = plt.subplots( nrows=1, ncols=1)#, figsize = (16,9) ) 
-            import csv
-            with open(os.path.join(current,'auc_roc.csv'), 'r') as readFile:
-                reader = csv.reader(readFile)
-                lines = list(reader)
-
-            lines = sorted(lines, key = (lambda x: x[0]))
-
-            dummy = np.Inf
-            dummyName = ""
-            dummyList = None
-            for line in lines:
-                if int(line[0].split(".")[-1]) == dummy:
-                    dummy += 1
-                    dummyList = dummyList + np.asarray([float(i) for i in line[1:]])
-                else:
-                    if dummyName != "":
-                        ax.plot(dummyList/dummy, label = dummyName)
-                    dummy = 1
-                    dummyList = np.asarray([float(i) for i in line[1:]])
-                    dummyName = ".".join(line[0].split(".")[:-1])
-                #if "random" in line[0] or True:
-                #    ax.plot([float(i) for i in line[1:]], label = line[0])
-            ax.plot(dummyList/dummy, label = dummyName)
-            plt.title('ROC AUC after each round', fontsize=20)
-            plt.ylabel('ROC AUC',fontsize=18)
-            plt.xlabel('Round i',fontsize=18)
-            plt.legend(fontsize = 16)
-            plt.ylim([0,1])
-            plt.xticks(fontsize = 13)
-            plt.yticks(fontsize = 13)
-            fig.savefig(os.path.join(current,'img/ROC AUC.png'),bbox_inches='tight')
-            plt.show()
-            plt.close(fig)
-
-            fig, ax = plt.subplots( nrows=1, ncols=1, figsize = (16,9) ) 
-            import csv
-            with open(os.path.join(current,'auc_pr.csv'), 'r') as readFile:
-                reader = csv.reader(readFile)
-                lines = list(reader)
-
-            for line in lines:
-                if "random" in line[0] or True:
-                    ax.plot([float(i) for i in line[1:]], label = line[0])
-            plt.title('PR AUC')
-            plt.ylabel('PR AUC')
-            plt.xlabel('Round i')
-            fig.legend()
-            #plt.show()
-            fig.savefig(os.path.join(current,'img/PR AUC.png'),bbox_inches='tight')
-            plt.close(fig)
-
-
-            ## BAGLEVEL
-
-            fig, ax = plt.subplots( nrows=1, ncols=1, figsize = (16,9) ) 
-            import csv
-            with open(os.path.join(current,'auc_roc_bag.csv'), 'r') as readFile:
-                reader = csv.reader(readFile)
-                lines = list(reader)
-
-            for line in lines:
-                if "random" in line[0] or True:
-                    ax.plot([float(i) for i in line[1:]], label = line[0])
-            plt.title('ROC AUC BAG-LEVEL')
-            plt.ylabel('ROC AUC')
-            plt.xlabel('Round i')
-            fig.savefig(os.path.join(current,'img/ROC AUC BAG.png'),bbox_inches='tight')
-            fig.legend()
-            #plt.show()
-            plt.close(fig)
-
-            fig, ax = plt.subplots( nrows=1, ncols=1, figsize = (16,9) ) 
-            import csv
-            with open(os.path.join(current,'auc_pr_bag.csv'), 'r') as readFile:
-                reader = csv.reader(readFile)
-                lines = list(reader)
-                    
-            for line in lines:
-                if "random" in line[0] or True:
-                    ax.plot([float(i) for i in line[1:]], label = line[0])
-            plt.title('PR AUC BAG-LEVEL')
-            plt.ylabel('PR AUC')
-            plt.xlabel('Round i')
-            fig.savefig(os.path.join(current,'img/PR AUC BAG.png'),bbox_inches='tight')
-            fig.legend()
-            #plt.show()
-            plt.close(fig)
-
-            ## TIMES
-
-            self.plotTimes2()
 
     def _fig(self, *args: Any, **kwds: Any) -> Any:
                     
@@ -309,6 +226,6 @@ class PlotPerformance:
             plt.close()
 
 if __name__=="__main__":
-    pp = PlotPerformance("33_skin")
+    pp = PlotPerformance("2_annthyroid0")
     pp()
     #pp._fig()
