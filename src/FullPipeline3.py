b434e055b2265f50f230f33c4c0e139f7e679487
Maarten Vanmarcke <maarten.vanmarcke@student.kuleuven.be>
2024-06-02 23:57:06 +0200

    Update FullPipeline3.py

--- src/FullPipeline3.py
+++ src/FullPipeline3.py
@@ -38,9 +38,9 @@ class FullPipeline:
         for inputData in inputDatas:
             normals = inputData.getNormals()
             anomalies = inputData.getAnomalies()
-            seedcounter = 31
+            seedcounter = 16
             query_budget = 100
-            for i in [14]:
+            for i in [7]:
                 """file = open(p, mode = "w")
                 file.write(str(i))
                 file.close() """  
