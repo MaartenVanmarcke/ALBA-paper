36c8ee463dadf2ee3c07d9ba8362e8f69277a784
Maarten Vanmarcke <maarten.vanmarcke@student.kuleuven.be>
2024-06-02 17:11:18 +0200

    Update inputData.py

--- src/pipeline/inputData.py
+++ src/pipeline/inputData.py
@@ -204,7 +204,7 @@ class Cardio_6(InputData):
 class checkData(InputData):
     
     def __init__(self) -> None:
-        name = "40_vowels"
+        name = "47_yeast"
         self.name = name 
         super().__init__(name)
 
